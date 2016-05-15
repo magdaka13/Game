0b1e53bc3c709ee2b8d99eb0deeb9d193ee9c043
magda <magdalena.kazmierczak@wp.pl>
2016-05-16 01:33:29 +0200

    upgraded letters game -added DB to ligbdx
    TODO:
    -screen with choosing profile and proper updating of DB
    -recognize if no records were added-then firstly insert users data
    -2nd part: recegnition of speech+shown letter

--- android/src/com/LettersGame/LettersGame/AlphabetFilm.java
+++ android/src/com/LettersGame/LettersGame/AlphabetFilm.java
@@ -1,40 +1,38 @@
 package com.LettersGame.LettersGame;
 
 import android.app.Activity;
-import android.media.MediaPlayer.OnCompletionListener;
-import android.media.MediaPlayer;
-import android.widget.VideoView;
-import android.net.Uri;
+import android.webkit.WebView;
 import android.os.Bundle;
-import android.content.Intent;
+
+
 /**
  * Created by magda on 2016-04-13.
  */
-public class AlphabetFilm extends Activity implements OnCompletionListener
-{
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+public class AlphabetFilm extends Activity {
+    //    private FrameLayout mContentView,mCustomViewContainer;
+    private WebView webView;
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+
+        String frameVideo = "<html><body>Youtube video .. <br> <iframe width='320' height='315' src='https://www.youtube.com/watch?v=TlP_sF3lZ_0&feature=player_embedded' frameborder='0' allowfullscreen></iframe></body></html>";
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.alphabet);
-        String fileName = "android.resource://"+  getPackageName() +"/"+ R.raw.literki;
 
-        VideoView vv = (VideoView) this.findViewById(R.id.surface);
-        vv.setVideoURI(Uri.parse(fileName));
-        vv.canSeekForward();
-        vv.setOnCompletionListener(this);
-        vv.start();
+        webView = (WebView) findViewById(R.id.webview1);
+
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setSupportMultipleWindows(false);
+        webView.getSettings().setSupportZoom(false);
+        webView.setVerticalScrollBarEnabled(false);
+        webView.setHorizontalScrollBarEnabled(false);
+
+        webView.loadUrl("https://www.youtube.com/watch?v=TlP_sF3lZ_0");
 
     }
 
-    @Override
-    public void onCompletion(MediaPlayer mp)
-    {
-        // TODO Auto-generated method stub
-        Intent intent = new Intent(this, AndroidLauncher.class);
-        startActivity(intent);
-        finish();
-    }
+
+
 }
